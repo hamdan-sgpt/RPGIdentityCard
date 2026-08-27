f2f4861a431eb9d24fa1e087e52cfbe5d0917258
hamdan-sgpt <hamdan130310ganteng@gmail.com>
2026-08-27 20:21:09 +0700

    Update create_texture.ps1 race texture colors

--- create_texture.ps1
+++ create_texture.ps1
@@ -1,91 +1,88 @@
+# PowerShell Native Image Generator Script for RPG Identity Card
 Add-Type -AssemblyName System.Drawing
 
 $baseDir = $PSScriptRoot
-if (-not $baseDir) { $baseDir = Get-Location }
+$textureDir = Join-Path $baseDir "resourcepack\assets\minecraft\textures\item"
 
-$logoPath = Join-Path $baseDir "logo.png"
-$namaPath = Join-Path $baseDir "nama.png"
-$outDir = Join-Path $baseDir "resourcepack\assets\minecraft\textures\item"
-
-if (-not (Test-Path $outDir)) {
-    New-Item -ItemType Directory -Path $outDir -Force | Out-Null
+if (-not (Test-Path $textureDir)) {
+    New-Item -ItemType Directory -Path $textureDir -Force | Out-Null
 }
 
-function Generate-RaceCardTexture($raceName, $borderR, $borderG, $borderB, $bgR, $bgG, $bgB, $fileName) {
-    $outPath = Join-Path $outDir $fileName
+function Generate-RaceCardTexture {
+    param(
+        [string]$raceTitle,
+        [int]$r, [int]$g, [int]$b,
+        [int]$bgR, [int]$bgG, [int]$bgB,
+        [string]$filename
+    )
 
-    $bmp = New-Object System.Drawing.Bitmap(256, 256)
-    $g = [System.Drawing.Graphics]::FromImage($bmp)
-    $g.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
+    $width = 128
+    $height = 128
+    $bmp = New-Object System.Drawing.Bitmap($width, $height)
+    $graphics = [System.Drawing.Graphics]::FromImage($bmp)
+    $graphics.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
+    $graphics.TextRenderingHint = [System.Drawing.Text.TextRenderingHint]::AntiAliasGridFit
 
-    # Background Color per Ras
+    # Background
     $bgBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255, $bgR, $bgG, $bgB))
-    $g.FillRectangle($bgBrush, 0, 0, 256, 256)
+    $graphics.FillRectangle($bgBrush, 0, 0, $width, $height)
 
-    # Border Color per Ras
-    $goldPen = New-Object System.Drawing.Pen([System.Drawing.Color]::FromArgb(255, $borderR, $borderG, $borderB), 3)
-    $bluePen = New-Object System.Drawing.Pen([System.Drawing.Color]::FromArgb(255, 97, 175, 239), 1)
-    $g.DrawRectangle($goldPen, 4, 4, 247, 247)
-    $g.DrawRectangle($bluePen, 8, 8, 239, 239)
+    # Outer Border (Gold / Race Color)
+    $borderPen = New-Object System.Drawing.Pen([System.Drawing.Color]::FromArgb(255, $r, $g, $b), 3)
+    $graphics.DrawRectangle($borderPen, 2, 2, 123, 123)
 
-    # Logo
-    if (Test-Path $logoPath) {
-        try {
-            $logo = [System.Drawing.Image]::FromFile($logoPath)
-            $g.DrawImage($logo, 14, 14, 50, 50)
-            $logo.Dispose()
-        } catch {}
-    }
+    # Inner Accent Border
+    $innerPen = New-Object System.Drawing.Pen([System.Drawing.Color]::FromArgb(255, 97, 175, 239), 1)
+    $graphics.DrawRectangle($innerPen, 5, 5, 117, 117)
 
-    # Nama Server
-    if (Test-Path $namaPath) {
-        try {
-            $nama = [System.Drawing.Image]::FromFile($namaPath)
-            $g.DrawImage($nama, 70, 16, 170, 45)
-            $nama.Dispose()
-        } catch {}
-    }
+    # Header Text
+    $fontHeader = New-Object System.Drawing.Font("Arial", 7, [System.Drawing.FontStyle]::Bold)
+    $fontSub = New-Object System.Drawing.Font("Arial", 5, [System.Drawing.FontStyle]::Regular)
+    $fontData = New-Object System.Drawing.Font("Courier New", 5, [System.Drawing.FontStyle]::Bold)
+    $fontFooter = New-Object System.Drawing.Font("Arial", 4, [System.Drawing.FontStyle]::Bold)
 
-    $g.DrawLine($goldPen, 14, 70, 242, 70)
-
-    # Frame Pas Foto 3x4
-    $photoBg = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255, 30, 38, 56))
-    $g.FillRectangle($photoBg, 14, 80, 70, 90)
-    $g.DrawRectangle($goldPen, 14, 80, 70, 90)
-
-    $font = New-Object System.Drawing.Font("Arial", 8, [System.Drawing.FontStyle]::Bold)
-    $whiteBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255, 255, 255, 255))
-    $goldBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255, $borderR, $borderG, $borderB))
-    $greenBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255, 152, 195, 121))
+    $goldBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255, $r, $g, $b))
+    $whiteBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::White)
     $grayBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255, 171, 178, 191))
+    $greenBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255, 152, 195, 121))
 
-    $g.DrawString("PAS FOTO`n  (3x4)", $font, $grayBrush, 18, 115)
+    $graphics.DrawString("KERAJAAN VALORIA", $fontHeader, $goldBrush, 18, 10)
+    $graphics.DrawString("KARTU IDENTITAS RPG RESMI", $fontSub, $grayBrush, 14, 20)
 
-    # Teks Metadata
-    $g.DrawString("ID   : ID-7492-9102", $font, $goldBrush, 92, 80)
-    $g.DrawString("VERIF: ASLI (RESMI)", $font, $greenBrush, 92, 98)
-    $g.DrawString("NAMA : RPG PLAYER", $font, $whiteBrush, 92, 118)
-    $g.DrawString("RAS  : $raceName", $font, $goldBrush, 92, 136)
-    $g.DrawString("CLASS: HIGH MAGE", $font, $whiteBrush, 92, 154)
+    # Line Separator
+    $linePen = New-Object System.Drawing.Pen([System.Drawing.Color]::FromArgb(255, $r, $g, $b), 1)
+    $graphics.DrawLine($linePen, 8, 28, 120, 28)
+
+    # Avatar Frame Box (32x32)
+    $graphics.FillRectangle($bgBrush, 10, 32, 32, 32)
+    $graphics.DrawRectangle($borderPen, 10, 32, 32, 32)
+    $graphics.DrawString("FOTO", $fontSub, $grayBrush, 14, 43)
+
+    # Data Attributes
+    $graphics.DrawString("ID   : ID-VALORIA", $fontData, $goldBrush, 46, 33)
+    $graphics.DrawString("VERIF: ASLI (RESMI)", $fontData, $greenBrush, 46, 41)
+    $graphics.DrawString("RAS  : " + $raceTitle, $fontData, $goldBrush, 46, 49)
+    $graphics.DrawString("CLASS: CITIZEN", $fontData, $whiteBrush, 46, 57)
+
+    # Line Separator 2
+    $graphics.DrawLine($linePen, 8, 68, 120, 68)
 
     # Footer
-    $footerBg = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255, 24, 32, 48))
-    $g.FillRectangle($footerBg, 14, 185, 228, 57)
-    $g.DrawRectangle($bluePen, 14, 185, 228, 57)
+    $graphics.DrawString("OFFICIAL ROYAL IDENTITY CARD", $fontFooter, $goldBrush, 14, 73)
+    $graphics.DrawString("VERIFIED BY ROYAL SYSTEM #2026", $fontFooter, $greenBrush, 10, 81)
 
-    $smallFont = New-Object System.Drawing.Font("Arial", 7, [System.Drawing.FontStyle]::Bold)
-    $g.DrawString("VALORIA OFFICIAL IDENTITY CARD", $smallFont, $goldBrush, 20, 195)
-    $g.DrawString("VERIFIED BY ROYAL REGISTRY SYSTEM", $smallFont, $greenBrush, 20, 215)
+    # Save PNG
+    $filePath = Join-Path $textureDir $filename
+    $bmp.Save($filePath, [System.Drawing.Imaging.ImageFormat]::Png)
 
-    $g.Dispose()
-    $bmp.Save($outPath, [System.Drawing.Imaging.ImageFormat]::Png)
+    $graphics.Dispose()
     $bmp.Dispose()
-    Write-Host "TERCIPTA TEKSTUR: $fileName"
+    Write-Host "TERCIPTA TEKSTUR: $filename"
 }
 
 # Generasi Tekstur per Ras
-Generate-RaceCardTexture "HUMAN" 200 200 200 18 24 38 "rpg_card_human.png"
+Generate-RaceCardTexture "HUMAN" 255 255 255 18 24 38 "rpg_card_human.png"
 Generate-RaceCardTexture "ELF" 46 204 113 14 36 24 "rpg_card_elf.png"
-Generate-RaceCardTexture "DWARF" 241 196 15 40 30 15 "rpg_card_dwarf.png"
+Generate-RaceCardTexture "DWARF" 230 126 34 40 25 15 "rpg_card_dwarf.png"
 Generate-RaceCardTexture "DEMON" 231 76 60 40 15 15 "rpg_card_demon.png"
-Generate-RaceCardTexture "RPG CITIZEN" 229 192 123 18 24 38 "rpg_identity_card.png"
+Generate-RaceCardTexture "RPG CITIZEN" 255 255 255 18 24 38 "rpg_identity_card.png"
