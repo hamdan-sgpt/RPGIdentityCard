d4ac6641b50fa6b0e892bd29fca7ceda8e064628
hamdan-sgpt <hamdan130310ganteng@gmail.com>
2026-08-28 21:28:53 +0700

    Add insufficient_funds_msg in PluginConfig.java

--- src/main/java/com/rpgidentity/config/PluginConfig.java
+++ src/main/java/com/rpgidentity/config/PluginConfig.java
@@ -8,12 +8,12 @@ import org.bukkit.configuration.file.FileConfiguration;
 public class PluginConfig {
 
     private final RPGIdentityPlugin plugin;
-    
+
     private String kingdom;
     private String city;
     private String statusBerlaku;
     private String secretKey;
-    
+
     private String prefix;
     private String noIdSelf;
     private String noIdOther;
@@ -33,6 +33,7 @@ public class PluginConfig {
     private int registrationCost;
     private String economyTakeCommand;
     private String paidSuccessMsg;
+    private String insufficientFundsMsg;
 
     public PluginConfig(RPGIdentityPlugin plugin) {
         this.plugin = plugin;
@@ -44,37 +45,42 @@ public class PluginConfig {
         plugin.reloadConfig();
         FileConfiguration config = plugin.getConfig();
 
-        this.kingdom = colorize(config.getString("header.kingdom", "KERAJAAN VALORIA - KARTU IDENTITAS RPG"));
-        this.city = colorize(config.getString("header.city", "KOTA ATLANTA CENTRAL"));
-        this.statusBerlaku = colorize(config.getString("header.status_berlaku", "SEUMUR HIDUP"));
-        this.secretKey = config.getString("security.secret_key", "ValoriaSecretKey2026");
+        kingdom = color(config.getString("card.kingdom", "&b&lKERAJAAN VALDORA"));
+        city = color(config.getString("card.city", "&7KOTA VALORIA"));
+        statusBerlaku = color(config.getString("card.status_berlaku", "&a&lSEUMUR HIDUP"));
+        secretKey = config.getString("card.secret_key", "VALDORA-SECRET-SIGNATURE-2026");
 
-        this.prefix = colorize(config.getString("messages.prefix", "&8[&b&lIDENTITY&8] &r"));
-        this.noIdSelf = colorize(config.getString("messages.no_id_self", "&cKamu belum mendaftar Kartu Identitas! Membuka formulir..."));
-        this.noIdOther = colorize(config.getString("messages.no_id_other", "&cPemain &e%player% &cbelum mendaftar Kartu Identitas."));
-        this.registrationSuccess = colorize(config.getString("messages.registration_success", "&a&lSELAMAT! Kartu Identitas kamu berhasil diterbitkan secara resmi."));
-        this.giveCardSuccess = colorize(config.getString("messages.give_card_success", "&aBerhasil mengambil Fisik Kartu Identitas milik &e%nama%&a!"));
-        this.showCardSender = colorize(config.getString("messages.show_card_sender", "&aKamu menunjukkan Kartu Identitas milikmu kepada &e%target%&a!"));
-        this.showCardReceiver = colorize(config.getString("messages.show_card_receiver", "&e%sender% &amenunjukkan Kartu Identitas miliknya kepadamu:"));
-        this.cardLocked = colorize(config.getString("messages.card_locked", "&cKartu Identitas kamu sudah diterbitkan secara resmi dan dikunci! Hubungi Admin jika ingin mengubah data."));
+        prefix = color(config.getString("messages.prefix", "&8[&bIDENTITY&8] "));
+        noIdSelf = color(config.getString("messages.no_id_self", "&cKamu belum mendaftarkan Kartu Identitas! Menampilkan form pendaftaran..."));
+        noIdOther = color(config.getString("messages.no_id_other", "&c%player% belum memiliki Kartu Identitas Resmi!"));
+        registrationSuccess = color(config.getString("messages.registration_success", "&aKartu Identitas Resmi milikmu telah berhasil terbit!"));
+        giveCardSuccess = color(config.getString("messages.give_card_success", "&aBerhasil mengambil Fisik Kartu Identitas milik &e%nama%&a!"));
+        showCardSender = color(config.getString("messages.show_card_sender", "&aKamu menunjukkan Kartu Identitas milikmu kepada &e%target%&a."));
+        showCardReceiver = color(config.getString("messages.show_card_receiver", "&e%sender% &atelah menunjukkan Kartu Identitas miliknya kepadamu."));
+        cardLocked = color(config.getString("messages.card_locked", "&cKartu Identitas kamu sudah terbit dan terkunci! Hanya Admin yang dapat me-edit data ini."));
 
-        String matStr = config.getString("item.material", "PAPER");
-        Material mat = Material.matchMaterial(matStr);
-        this.itemMaterial = mat != null ? mat : Material.PAPER;
-        this.customModelData = config.getInt("item.custom_model_data", 900001);
-        this.playerCardCmdStart = config.getInt("item.player_card_cmd_start", 900100);
-        this.useCustomMap = config.getBoolean("item.use_custom_map", false);
-        this.autoIazip = config.getBoolean("item.auto_iazip", true);
+        String matName = config.getString("item.material", "PAPER");
+        try {
+            itemMaterial = Material.valueOf(matName.toUpperCase());
+        } catch (Exception e) {
+            itemMaterial = Material.PAPER;
+        }
 
-        this.economyEnabled = config.getBoolean("economy.enabled", true);
-        this.registrationCost = config.getInt("economy.cost", 100);
-        this.economyTakeCommand = config.getString("economy.take_command", "economy take %player% %cost%");
-        this.paidSuccessMsg = colorize(config.getString("economy.paid_success_msg", "&aBiaya penerbitan KTP sebesar &e%cost% Koin &atelah dipotong!"));
+        customModelData = config.getInt("item.custom_model_data", 900001);
+        playerCardCmdStart = config.getInt("item.player_card_cmd_start", 900100);
+        useCustomMap = config.getBoolean("item.use_custom_map", false);
+        autoIazip = config.getBoolean("item.auto_iazip", true);
+
+        economyEnabled = config.getBoolean("economy.enabled", true);
+        registrationCost = config.getInt("economy.cost", 100);
+        economyTakeCommand = config.getString("economy.take_command", "economy take %player% %cost%");
+        paidSuccessMsg = color(config.getString("economy.paid_success_msg", "&aBiaya penerbitan KTP sebesar &e%cost% Koin &atelah dipotong!"));
+        insufficientFundsMsg = color(config.getString("economy.insufficient_funds_msg", "&c&l[❌] Koin kamu tidak cukup! Kamu membutuhkan &e%cost% Koin &cuntuk membuat KTP!"));
     }
 
-    public String colorize(String text) {
-        if (text == null) return "";
-        return ChatColor.translateAlternateColorCodes('&', text);
+    private String color(String s) {
+        if (s == null) return "";
+        return ChatColor.translateAlternateColorCodes('&', s);
     }
 
     public String getKingdom() { return kingdom; }
@@ -99,4 +105,5 @@ public class PluginConfig {
     public int getRegistrationCost() { return registrationCost; }
     public String getEconomyTakeCommand() { return economyTakeCommand; }
     public String getPaidSuccessMsg() { return prefix + paidSuccessMsg.replace("%cost%", String.valueOf(registrationCost)); }
+    public String getInsufficientFundsMsg() { return prefix + insufficientFundsMsg.replace("%cost%", String.valueOf(registrationCost)); }
 }
